883ffd6cb71460721c42745e28029fd29fb86db9
Rob Taylor <63524882+dataforyounz@users.noreply.github.com>
2022-12-20 07:59:26 +1300

    Update streamlit_app.py
    Added user selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,11 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruitvice API response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# User defined selectecion
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice )
 
 # normalise json response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
